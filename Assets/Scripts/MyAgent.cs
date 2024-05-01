15b2b941692977dc204871655d2f0d360f446109
sz-attila <szalai.attila2002@gmail.com>
2024-05-01 17:31:04 +0200

    Tried to rework agent movement and shooting to work while training.

--- Assets/Scripts/MyAgent.cs
+++ Assets/Scripts/MyAgent.cs
@@ -1,51 +1,97 @@
-using Unity.VisualScripting;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
 using UnityEngine;
 
-public class MyAgent : MonoBehaviour
+public class MyAgent : Agent
 {
     public float speed = 3f;
+    public GameObject bulletPrefab;
 
     private Rigidbody rb;
     private Animator animator;
     private bool isMoving = false;
+    private GameObject bullet;
 
-    public GameObject bullet;
-
-    private void Start()
+    public override void Initialize()
     {
         rb = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
+        bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
         bullet.SetActive(false);
     }
 
-    private void Update()
+    public override void OnEpisodeBegin()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+        rb.velocity = Vector3.zero;
+        bullet.SetActive(false);
+    }
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            ShootBullet();
-        }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(transform.forward);
+    }
 
-        SoldierRotation(moveHorizontal, moveVertical);
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float moveHorizontal = actions.ContinuousActions[0];
+        float moveVertical = actions.ContinuousActions[1];
+        int shootAction = actions.DiscreteActions[0];
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.velocity = movement * speed;
 
         isMoving = movement.magnitude > 0;
 
+        SoldierRotation(moveHorizontal, moveVertical);
+
         animator.SetBool("IsRunning", isMoving);
+
+        if (shootAction == 1)
+        {
+            ShootBullet();
+        }
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxis("Horizontal");
+        continuousActions[1] = Input.GetAxis("Vertical");
+
+        var discreteActions = actionsOut.DiscreteActions;
+        discreteActions[0] = Input.GetKey(KeyCode.Space) ? 1 : 0;
     }
 
     private void ShootBullet()
     {
-        GameObject bulletInstance = Instantiate(bullet, bullet.transform.position, bullet.transform.rotation);
-        bulletInstance.SetActive(true);
+        float maxDistance = 100f;
+        RaycastHit hit;
 
-        Rigidbody bulletRb = bulletInstance.GetComponent<Rigidbody>();
-        bulletRb.velocity = transform.forward * 10;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, maxDistance))
+        {
+            Debug.Log("Hit object: " + hit.collider.gameObject.name + ", Layer: " + LayerMask.LayerToName(hit.collider.gameObject.layer));
+
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+            {
+                bullet.SetActive(true);
+                bullet.transform.position = transform.position + transform.forward * 2;
+                bullet.transform.rotation = transform.rotation;
+
+                Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
+                bulletRb.velocity = transform.forward * 10;
+
+
+                return;
+            }
+        }
+
     }
+
+
 
     private void SoldierRotation(float horizontal, float vertical)
     {
